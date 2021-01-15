a2298776358c2de733c4e1d64f3357cba40a680c
neha vardekar <vardekarneha@gmail.com>
2021-01-15 11:22:24 +0530

    empcate changes in repository

--- src/main/java/com/upskillutoday/crmRoot/repository/EmpCategyRepository.java
+++ src/main/java/com/upskillutoday/crmRoot/repository/EmpCategyRepository.java
@@ -1,10 +1,16 @@
 package com.upskillutoday.crmRoot.repository;
 
+import com.upskillutoday.crmRoot.model.CategoryMaster;
 import com.upskillutoday.crmRoot.model.EmpCategy;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 // Added By Laukik
 @Repository
 public interface EmpCategyRepository extends CrudRepository<EmpCategy, Long> {
+
+	List<EmpCategy> findByCategoryMaster(CategoryMaster categoryMaster);
 }
